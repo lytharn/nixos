03199b9b646ed3c2eed03afcf4d04c8cf02a7df5
lytharn <lytharn@users.noreply.github.com>
2024-07-25 08:55:49 +0200

    mewx: Add fwupd service to update firmware

--- mewx/configuration.nix
+++ mewx/configuration.nix
@@ -84,6 +84,22 @@
 
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ];
+ 
+  # Enable firmware updates
+  # 
+  # USAGE:
+  # To display all devices detected by fwupd:
+  # $ fwupdmgr get-devices
+  #
+  # To download the latest metadata from the Linux Vendor firmware Service (LVFS):
+  # $ fwupdmgr refresh
+  #
+  # To list updates available for any devices on the system:
+  # $ fwupdmgr get-updates
+  #
+  # To install updates:
+  # $ fwupdmgr update
+  services.fwupd.enable = true;
 
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.stable;
