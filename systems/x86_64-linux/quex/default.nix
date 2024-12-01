5d47be2cd4fbfe90e2e2534a195fa12ddb11df72
lytharn <lytharn@users.noreply.github.com>
2024-12-01 08:49:56 +0100

    Update flake lock file
    Fix font config

--- systems/x86_64-linux/quex/default.nix
+++ systems/x86_64-linux/quex/default.nix
@@ -111,7 +111,9 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  fonts.packages = with pkgs; [ (nerdfonts.override { fonts = [ "Hack" ]; }) ];
+  fonts.packages = with pkgs; [
+    nerd-fonts.hack
+  ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.lytharn = {
