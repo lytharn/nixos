fe185ca88b91d6737fe9d8b62396db8d012804c1
lytharn <lytharn@users.noreply.github.com>
2024-12-22 07:55:33 +0100

    Extract tmux to module

--- homes/x86_64-linux/lytharn@quex/default.nix
+++ homes/x86_64-linux/lytharn@quex/default.nix
@@ -181,7 +181,10 @@
     };
   };
 
-  slask.apps.alacritty.enable = true;
+  slask.apps = {
+    alacritty.enable = true;
+    tmux.enable = true;
+  };
 
   programs.kitty = {
     enable = true;
@@ -225,51 +228,6 @@
         cursor-shape.insert = "bar";
       };
     };
-  };
-
-  programs.tmux = {
-    enable = true;
-    aggressiveResize = true;
-    baseIndex = 1;
-    clock24 = true;
-    escapeTime = 0;
-    historyLimit = 100000;
-    prefix = "C-a";
-    sensibleOnTop = true;
-    shell = "${pkgs.fish}/bin/fish";
-    terminal = "tmux-256color";
-    plugins = with pkgs; [ tmuxPlugins.pain-control ];
-    extraConfig = ''
-      # Set vi copy mode
-      setw -g mode-keys vi
-
-      # Fix true color support
-      set-option -sa terminal-overrides ',alacritty:RGB'
-
-      # Set status line
-      set -g status-justify "left"
-      set -g status "on"
-      set -g status-left-style "none"
-      set -g message-command-style "fg=colour0,bg=colour4"
-      set -g status-right-style "none"
-      set -g pane-active-border-style "fg=colour6"
-      set -g status-style "none,bg=colour8"
-      set -g message-style "fg=colour0,bg=colour4"
-      set -g pane-border-style "fg=colour4"
-      set -g status-right-length "100"
-      set -g status-left-length "100"
-      setw -g window-status-activity-style "none"
-      setw -g window-status-separator ""
-      setw -g window-status-style "none,fg=colour7,bg=colour8"
-      set -g status-left "#[fg=colour0,bg=colour6] #S #[fg=colour6,bg=colour8]"
-      set -g status-right "#[fg=colour4,bg=colour8]#[fg=colour0,bg=colour4] %Y-%m-%d  %H:%M #[fg=colour6,bg=colour4]#[fg=colour0,bg=colour6] #h "
-      setw -g window-status-format "#[fg=colour7,bg=colour8] #I #[fg=colour7,bg=colour8] #W "
-      setw -g window-status-current-format "#[fg=colour8,bg=colour4]#[fg=colour0,bg=colour4] #I #[fg=colour0,bg=colour4] #W #[fg=colour4,bg=colour8]"
-
-      # Keybindings
-      bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel
-      bind-key -T copy-mode-vi v send-keys -X begin-selection
-    '';
   };
 
   programs.git = {
---
+++ modules/home/apps/tmux/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  config,
+  pkgs,
+  namespace,
+  ...
+}:
+let
+  cfg = config.${namespace}.apps.tmux;
+in
+{
+  options.${namespace}.apps.tmux = {
+    enable = lib.mkEnableOption "tmux";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.tmux = {
+      enable = true;
+      aggressiveResize = true;
+      baseIndex = 1;
+      clock24 = true;
+      escapeTime = 0;
+      historyLimit = 100000;
+      prefix = "C-a";
+      sensibleOnTop = true;
+      shell = "${pkgs.fish}/bin/fish";
+      terminal = "tmux-256color";
+      plugins = with pkgs; [ tmuxPlugins.pain-control ];
+      extraConfig = builtins.readFile ./tmux.conf; 
+    };
+  };
+}
