5c2d2c8e29f393f6379192d172221f439431ef32
lytharn <lytharn@users.noreply.github.com>
2024-12-23 05:09:58 +0100

    Extract git to module

--- homes/x86_64-linux/lytharn@quex/default.nix
+++ homes/x86_64-linux/lytharn@quex/default.nix
@@ -183,6 +183,7 @@
 
   slask.apps = {
     alacritty.enable = true;
+    git.enable = true;
     tmux.enable = true;
   };
 
@@ -226,39 +227,6 @@
       editor = {
         cursorline = true;
         cursor-shape.insert = "bar";
-      };
-    };
-  };
-
-  programs.git = {
-    enable = true;
-    aliases = {
-      co = "checkout";
-      cp = "cherry-pick";
-      st = "status";
-      amend = "commit --amend --no-edit";
-      l = "log --graph --pretty=customone";
-      lt = "log --graph --pretty=customone --first-parent";
-      ltr = "log -15 --reverse --pretty=customone --first-parent";
-      ll = "log --graph --pretty=customfull";
-      rl = "reflog --pretty=customref";
-      rll = "reflog --pretty=customreffull";
-    };
-    userEmail = "lytharn@users.noreply.github.com";
-    userName = "lytharn";
-    difftastic.enable = true;
-    extraConfig = {
-      core = {
-        editor = "nvim";
-      };
-      merge = {
-        conflictstyle = "diff3";
-      };
-      pretty = {
-        customone = "format:%C(yellow)%h %C(reset)%s %C(blue)%an %C(green)(%cr) %C(magenta)%d";
-        customfull = "format:Commit: %C(yellow)%H %C(magenta)%d%nAuthor: %C(bold blue)'%an' <%ae> %C(bold green)(%ai)%nCommitter: %C(blue)'%cn' <%ce> %C(green)(%ci)%n%B";
-        customrefone = "format:%C(yellow)%h %C(magenta)%gd %C(reset)%s %C(green)(%cr) %C(magenta)%d";
-        customreffull = "format:Selector: %C(magenta)%gD%nCommit: %C(yellow)%H %C(magenta)%d%nAuthor: %C(bold blue)'%an' <%ae> %C(bold green)(%ai)%nCommitter: %C(blue)'%cn' <%ce> %C(green)(%ci)%n%B";
       };
     };
   };
