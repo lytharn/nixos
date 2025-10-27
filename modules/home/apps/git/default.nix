09dd5d3f8935160f4b27e6ef88e3f403deb7b115
lytharn <lytharn@users.noreply.github.com>
2025-10-27 06:38:42 +0100

    Update flake lock file and fix git settings

--- modules/home/apps/git/default.nix
+++ modules/home/apps/git/default.nix
@@ -14,32 +14,35 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    programs.git = {
-      enable = true;
-      aliases = {
-        co = "checkout";
-        cp = "cherry-pick";
-        st = "status";
-        amend = "commit --amend --no-edit";
-        l = "log --graph --pretty=customone";
-        lt = "log --graph --pretty=customone --first-parent";
-        ltr = "log -15 --reverse --pretty=customone --first-parent";
-        ll = "log --graph --pretty=customfull";
-        rl = "reflog --pretty=customref";
-        rll = "reflog --pretty=customreffull";
+    programs = {
+      difftastic = {
+        enable = true;
+        git.enable = true;
       };
-      userEmail = "lytharn@users.noreply.github.com";
-      userName = "lytharn";
-      difftastic.enable = true;
-      extraConfig = {
-        merge = {
-          conflictstyle = "diff3";
-        };
-        pretty = {
-          customone = "format:%C(yellow)%h %C(reset)%s %C(blue)%an %C(green)(%cr) %C(magenta)%d";
-          customfull = "format:Commit: %C(yellow)%H %C(magenta)%d%nAuthor: %C(bold blue)'%an' <%ae> %C(bold green)(%ai)%nCommitter: %C(blue)'%cn' <%ce> %C(green)(%ci)%n%B";
-          customrefone = "format:%C(yellow)%h %C(magenta)%gd %C(reset)%s %C(green)(%cr) %C(magenta)%d";
-          customreffull = "format:Selector: %C(magenta)%gD%nCommit: %C(yellow)%H %C(magenta)%d%nAuthor: %C(bold blue)'%an' <%ae> %C(bold green)(%ai)%nCommitter: %C(blue)'%cn' <%ce> %C(green)(%ci)%n%B";
+      git = {
+        enable = true;
+        settings = {
+          alias = {
+            co = "checkout";
+            cp = "cherry-pick";
+            st = "status";
+            amend = "commit --amend --no-edit";
+            l = "log --graph --pretty=customone";
+            lt = "log --graph --pretty=customone --first-parent";
+            ltr = "log -15 --reverse --pretty=customone --first-parent";
+            ll = "log --graph --pretty=customfull";
+            rl = "reflog --pretty=customref";
+            rll = "reflog --pretty=customreffull";
+          };
+          user.email = "lytharn@users.noreply.github.com";
+          user.name = "lytharn";
+          merge.conflictstyle = "diff3";
+          pretty = {
+            customone = "format:%C(yellow)%h %C(reset)%s %C(blue)%an %C(green)(%cr) %C(magenta)%d";
+            customfull = "format:Commit: %C(yellow)%H %C(magenta)%d%nAuthor: %C(bold blue)'%an' <%ae> %C(bold green)(%ai)%nCommitter: %C(blue)'%cn' <%ce> %C(green)(%ci)%n%B";
+            customrefone = "format:%C(yellow)%h %C(magenta)%gd %C(reset)%s %C(green)(%cr) %C(magenta)%d";
+            customreffull = "format:Selector: %C(magenta)%gD%nCommit: %C(yellow)%H %C(magenta)%d%nAuthor: %C(bold blue)'%an' <%ae> %C(bold green)(%ai)%nCommitter: %C(blue)'%cn' <%ce> %C(green)(%ci)%n%B";
+          };
         };
       };
     };
