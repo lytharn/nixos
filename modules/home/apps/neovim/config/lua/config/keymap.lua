cef790e315699d9b7c153a167e25d96e0075ec52
lytharn <lytharn@users.noreply.github.com>
2026-06-21 20:56:19 +0200

    nvim: Use vim tmux navigator

--- modules/home/apps/neovim/config/lua/config/keymap.lua
+++ modules/home/apps/neovim/config/lua/config/keymap.lua
@@ -1,12 +1,6 @@
 -- Clear highlight search by escaping in normal mode
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Better window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to window left of current" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to window below" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to window above" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to window right of current" })
-
 -- Open float
 vim.keymap.set("n", "<leader>od", vim.diagnostic.open_float, { desc = "Open diagnostic" })
 
