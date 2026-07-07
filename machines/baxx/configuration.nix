0ab9732e2279ee47c555c755e91c1d533281660c
lytharn <lytharn@users.noreply.github.com>
2026-07-07 19:26:11 +0200

    clan: Add Home-Manager to serx and baxx
    Finish the deferred HM: wire home-manager into serx/baxx clan configs
    (like quex/mewx) for the shell tooling, replacing their orphaned
    Snowfall homes. Only homes/lytharn@standalone remains (a real non-NixOS
    config, kept as a flake output).

--- machines/baxx/configuration.nix
+++ machines/baxx/configuration.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  inputs,
   ...
 }:
 
@@ -13,6 +14,7 @@
     ../../modules/nixos/apps/neovim
     ../../modules/nixos/services/restic-server
     ../../clan/restic-secrets.nix
+    inputs.home-manager.nixosModules.home-manager
   ];
 
   # Bootloader.
@@ -159,6 +161,39 @@
     "nix-command"
     "flakes"
   ];
+
+  # Home-Manager for the shell tooling used when logged in to baxx. The home app modules under
+  # modules/home/apps are imported via clan/home-modules.nix, with namespace injected.
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "hm-bak"; # don't fail the first switch on pre-existing dotfiles
+    extraSpecialArgs = {
+      namespace = "slask";
+      inherit inputs;
+    };
+    users.lytharn = {
+      imports = [ ../../clan/home-modules.nix ];
+
+      home.username = "lytharn";
+      home.homeDirectory = "/home/lytharn";
+
+      slask.apps = {
+        bat.enable = true;
+        direnv.enable = true;
+        eza.enable = true;
+        fzf.enable = true;
+        git.enable = true;
+        helix.enable = true;
+        starship.enable = true;
+        tmux.enable = true;
+        zoxide.enable = true;
+      };
+
+      home.stateVersion = "25.05"; # DO NOT TOUCH
+      programs.home-manager.enable = true;
+    };
+  };
 
   system.stateVersion = "25.05"; # DO NOT TOUCH
 }
