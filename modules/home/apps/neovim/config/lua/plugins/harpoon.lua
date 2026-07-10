999a19f2926c3bb19b597f1693d8edb1dd4e2d52
lytharn <lytharn@users.noreply.github.com>
2026-07-10 08:31:04 +0200

    neovim: Port harpoon to v2 (harpoon2 branch)
    v1 is frozen; move to the actively-developed harpoon2 branch. Rewrite
    to the stateful API: harpoon:setup() in config, harpoon:list():add() /
    :select(n) for marks, and harpoon.ui:toggle_quick_menu(harpoon:list())
    for the menu. Keymaps unchanged (<leader>ja/jq/jw/je/jr/jl). Lock moved
    to the harpoon2 branch; verified the plugin loads and the API resolves.

--- modules/home/apps/neovim/config/lua/plugins/harpoon.lua
+++ modules/home/apps/neovim/config/lua/plugins/harpoon.lua
@@ -1,15 +1,22 @@
 return {
   "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
-  config = true,
+  config = function()
+    require("harpoon"):setup()
+  end,
   keys = {
-    { "<leader>ja", function() require("harpoon.mark").add_file() end,        desc = "Mark file with harpoon" },
-    { "<leader>jq", function() require("harpoon.ui").nav_file(1) end,         desc = "Go to harpoon mark 1" },
-    { "<leader>jw", function() require("harpoon.ui").nav_file(2) end,         desc = "Go to harpoon mark 2" },
-    { "<leader>je", function() require("harpoon.ui").nav_file(3) end,         desc = "Go to harpoon mark 3" },
-    { "<leader>jr", function() require("harpoon.ui").nav_file(4) end,         desc = "Go to harpoon mark 4" },
-    { "<leader>jl", function() require("harpoon.ui").toggle_quick_menu() end, desc = "Show harpoon mark list" },
+    { "<leader>ja", function() require("harpoon"):list():add() end,    desc = "Mark file with harpoon" },
+    { "<leader>jq", function() require("harpoon"):list():select(1) end, desc = "Go to harpoon mark 1" },
+    { "<leader>jw", function() require("harpoon"):list():select(2) end, desc = "Go to harpoon mark 2" },
+    { "<leader>je", function() require("harpoon"):list():select(3) end, desc = "Go to harpoon mark 3" },
+    { "<leader>jr", function() require("harpoon"):list():select(4) end, desc = "Go to harpoon mark 4" },
+    {
+      "<leader>jl",
+      function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end,
+      desc = "Show harpoon mark list",
+    },
   },
 }
